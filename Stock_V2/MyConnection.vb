ceb16fed5ab82648dba8a505805569101d304190
adhu <adhu@ADHU-PC>
2017-06-15 09:36:33 +0500

    db connect

---
+++ Stock_V2/MyConnection.vb
@@ -0,0 +1,7 @@
+﻿Public Class MyConnection
+
+
+    Public Shared MYconnectionstring As String = "Server=85.10.205.173,3306\SQLEXPRESS; Database=LBMS; User Id=adhude; Password=HebJ48xl;"
+
+
+End Class
